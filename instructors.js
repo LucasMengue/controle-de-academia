48ea9a3e39f1a9e2b1fdd5e0fdca50d59d88ccde
lucas <lucasmengue96@hotmail.com>
2021-04-06 16:44:42 -0300

    :package: instructors age logic

--- instructors.js
+++ instructors.js
@@ -13,11 +13,29 @@ exports.show = function (req, res) {
     return res.send("Instructor not found!");
   }
 
+  function age(timestamp) {
+    const today = new Date();
+    const birthDate = new Date(timestamp);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const month = today.getMonth() - birthDate.getMonth();
+
+    // age : 34
+    // today nov : 10
+    // birth nov : 10
+
+    if (month < 0 || (month == 0 && today.getDate() <= birthDate.getDate())) {
+      age = age - 1;
+    }
+
+    return age;
+  }
+
   const instructor = {
     ...foundInstructor,
-    age: "",
+    age: age(foundInstructor.birth),
     gender: "",
-    services: "",
+    services: foundInstructor.services.split(","),
     created_at: "",
   };
 
