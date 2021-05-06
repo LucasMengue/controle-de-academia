24002edef966c42b9b9da3ab352ff4d68f250c75
lucas <lucasmengue96@hotmail.com>
2021-05-06 16:38:58 -0300

    :package: sorting instructors in alphabetical order

--- src/app/models/Instructor.js
+++ src/app/models/Instructor.js
@@ -3,11 +3,14 @@ const { age, date } = require("../../lib/utils");
 
 module.exports = {
   all(callback) {
-    db.query(`SELECT * FROM instructors`, function (err, results) {
-      if (err) throw `Database Error! ${err}`;
+    db.query(
+      `SELECT * FROM instructors ORDER BY name ASC`,
+      function (err, results) {
+        if (err) throw `Database Error! ${err}`;
 
-      callback(results.rows);
-    });
+        callback(results.rows);
+      }
+    );
   },
   create(data, callback) {
     const query = `
