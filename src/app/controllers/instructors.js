d65de8e48f3ab9d442b1eca370a856d140d1fa71
lucas <lucasmengue96@hotmail.com>
2021-05-06 15:18:35 -0300

    :package: receiving datas from Instructor.js

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,9 +1,11 @@
+const Instructor = require("../models/Instructor");
 const { age, date } = require("../../lib/utils");
-const db = require("../../config/db");
 
 module.exports = {
   index(req, res) {
-    return res.render("instructors/index");
+    Instructor.all(function (instructors) {
+      return res.render("instructors/index", { instructors });
+    });
   },
   create(req, res) {
     return res.render("instructors/create");
@@ -17,35 +19,20 @@ module.exports = {
       }
     }
 
-    const query = `
-      INSERT INTO instructors (
-        name,
-        avatar_url,
-        gender,
-        services,
-        birth,
-        created_at
-      ) VALUES ($1, $2, $3, $4, $5, $6)
-      RETURNING id
-    `;
-
-    const values = [
-      req.body.name,
-      req.body.avatar_url,
-      req.body.gender,
-      req.body.services,
-      date(req.body.birth).iso,
-      date(Date.now()).iso,
-    ];
-
-    db.query(query, values, function (err, results) {
-      if (err) return res.send("Database Error!");
-
-      return res.redirect(`/instructors/${results.rows[0].id}`);
+    Instructor.create(req.body, function (instructor) {
+      return res.redirect(`/instructors/${instructor.id}`);
     });
   },
   show(req, res) {
-    return;
+    Instructor.find(req.params.id, function (instructor) {
+      if (!instructor) return res.send("Instructor not found!");
+
+      instructor.age = age(instructor.birth);
+      instructor.services = instructor.services.split(",");
+      instructor.created_at = date(instructor.created_at).format;
+
+      return res.render("instructors/show", { instructor });
+    });
   },
   edit(req, res) {
     return;
