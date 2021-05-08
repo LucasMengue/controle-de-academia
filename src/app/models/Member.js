0afcb0eb1c155ad8d8f5c60ece9e407c32b7c390
lucas <lucasmengue96@hotmail.com>
2021-05-08 19:59:34 -0300

    :package: added new object instructorSelectOptions to select a name and id from instructors

--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -22,8 +22,9 @@ module.exports = {
       birth,
       blood,
       weight,
-      height
-    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      height,
+      instructor_id
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
     RETURNING id
   `;
 
@@ -36,6 +37,7 @@ module.exports = {
       data.blood,
       data.weight,
       data.height,
+      data.instructor,
     ];
 
     db.query(query, values, function (err, results) {
@@ -46,9 +48,10 @@ module.exports = {
   },
   find(id, callback) {
     db.query(
-      `SELECT * 
+      `SELECT members.*, instructors.name AS instructor_name 
     FROM members
-     WHERE id = $1`,
+    LEFT JOIN instructors ON (members.instructor_id = instructors.id)
+     WHERE members.id = $1`,
       [id],
       function (err, results) {
         if (err) throw `Database Error! ${err}`;
@@ -67,7 +70,8 @@ module.exports = {
         blood=($6),
         weight=($7),
         height=($8),
-        WHERE id = $9
+        instructor_id=($9)
+        WHERE id = $10
       `;
 
     const values = [
@@ -79,6 +83,7 @@ module.exports = {
       data.blood,
       data.weight,
       data.height,
+      data.instructor,
       data.id,
     ];
 
@@ -98,6 +103,18 @@ module.exports = {
         if (err) throw `Database Error! ${err}`;
 
         return callback();
+      }
+    );
+  },
+  instructorsSelectOptions(callback) {
+    db.query(
+      `
+      SELECT name, id FROM instructors
+    `,
+      function (err, results) {
+        if (err) throw "Database Error!";
+
+        callback(results.rows);
       }
     );
   },
