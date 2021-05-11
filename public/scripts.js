053b8e8d274d39547d61abc94d1425df688f8ee3
lucas <lucasmengue96@hotmail.com>
2021-05-11 15:39:45 -0300

    :package: doing the paging logic on the front

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,33 @@ for (item of menuItems) {
     item.classList.add("active");
   }
 }
+
+// Paginação
+// totalPages = 20
+// selectedPages = 15
+// [1, ..., 13, 14, 15, 16, 17, ..., 20]
+
+let totalPages = 20,
+  selectedPage = 15,
+  pages = [],
+  oldPage;
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+  const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+  const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+  const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+
+  if (firstAndLastPage || (pagesBeforeSelectedPage && pagesAfterSelectedPage)) {
+    if (oldPage && currentPage - oldPage > 2) {
+      pages.push("...");
+    }
+
+    if (oldPage && currentPage - oldPage == 2) {
+      pages.push(oldPage + 1);
+    }
+
+    pages.push(currentPage);
+
+    oldPage = currentPage;
+  }
+}
