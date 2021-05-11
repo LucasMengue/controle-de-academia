2574ff0f2b092b36d8f59f522b39ded92f44d794
lucas <lucasmengue96@hotmail.com>
2021-05-11 16:29:34 -0300

    :package: created paginate function and used to show the pagination in index.njk

--- public/scripts.js
+++ public/scripts.js
@@ -12,27 +12,49 @@ for (item of menuItems) {
 // selectedPages = 15
 // [1, ..., 13, 14, 15, 16, 17, ..., 20]
 
-let totalPages = 20,
-  selectedPage = 15,
-  pages = [],
-  oldPage;
+function paginate(selectedPage, totalPages) {
+  let pages = [],
+    oldPage;
 
-for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
-  const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
-  const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
-  const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
 
-  if (firstAndLastPage || (pagesBeforeSelectedPage && pagesAfterSelectedPage)) {
-    if (oldPage && currentPage - oldPage > 2) {
-      pages.push("...");
+    if (
+      firstAndLastPage ||
+      (pagesBeforeSelectedPage && pagesAfterSelectedPage)
+    ) {
+      if (oldPage && currentPage - oldPage > 2) {
+        pages.push("...");
+      }
+
+      if (oldPage && currentPage - oldPage == 2) {
+        pages.push(oldPage + 1);
+      }
+
+      pages.push(currentPage);
+
+      oldPage = currentPage;
     }
+  }
 
-    if (oldPage && currentPage - oldPage == 2) {
-      pages.push(oldPage + 1);
-    }
+  return pages;
+}
 
-    pages.push(currentPage);
+const pagination = document.querySelector(".pagination");
+const page = +pagination.dataset.page;
+const total = +pagination.dataset.total;
+const pages = paginate(page, total);
 
-    oldPage = currentPage;
+let elements = "";
+
+for (let page of pages) {
+  if (String(page).includes("...")) {
+    elements += `<span>${page}</span>`;
+  } else {
+    elements += `<a href="?page=${page}">${page}</a>`;
   }
 }
+
+pagination.innerHTML = elements;
